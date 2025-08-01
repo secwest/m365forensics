dfca4728825a54edb9cdd0fbdb54a8d2874a4c19
Secwest.net <70259601+secwest@users.noreply.github.com>
2025-07-31 22:48:59 -0700

    Update OutlookStart.cmd

--- OutlookStart.cmd
+++ OutlookStart.cmd
@@ -2,4 +2,8 @@ Remove-Item "$env:LOCALAPPDATA\Microsoft\Outlook\*.xml" -Force
 reg delete HKCU\Software\Microsoft\Office\16.0\Outlook\AutoDiscover /f
 reg delete HKCU\Software\Microsoft\Office\16.0\Outlook\Profiles /f
 reg delete "HKCU\Software\Microsoft\OneDrive\Accounts" /f
+reg delete "HKCU\Software\Microsoft\OneDrive\Tenants" /f
+reg delete "HKCU\Software\Microsoft\OneDrive\IdentityCache" /f
+reg delete "HKCU\Software\Microsoft\OneDrive\Business1" /f
+"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /resetnavpane /resetfolders
 "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /profiles
