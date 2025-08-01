b6a282b30dcd9d27e61e9a5f78469e48200dcf4c
Secwest.net <70259601+secwest@users.noreply.github.com>
2025-07-31 22:54:24 -0700

    Update OutlookStart.cmd

--- OutlookStart.cmd
+++ OutlookStart.cmd
@@ -8,3 +8,6 @@ reg delete "HKCU\Software\Microsoft\OneDrive\Business1" /f
 "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /signout
 "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /resetnavpane /resetfolders
 "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /profiles
+
+# Repair Office installation
+"C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeClickToRun.exe" scenario=Repair platform=x64 culture=en-us RepairType=QuickRepair
