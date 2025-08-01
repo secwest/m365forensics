bd585c194958000e09b3962540913087314eb591
Secwest.net <70259601+secwest@users.noreply.github.com>
2025-07-31 22:45:28 -0700

    Update OutlookStart.cmd

--- OutlookStart.cmd
+++ OutlookStart.cmd
@@ -1,1 +1,3 @@
+Remove-Item "$env:LOCALAPPDATA\Microsoft\Outlook\*.xml" -Force
+ reg delete "HKCU\Software\Microsoft\OneDrive\Accounts" /f
 "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /profiles
